4e131b538b4dfb617067ebc12afabfc8a880959c
Matt Bostock <matt@mattbostock.com>
2014-05-23 10:03:15 +0100

    Load balancer integration test
    Add an integration test to configure a load balancer service on a
    vShield Edge Gateway.

--- spec/integration/edge_gateway/configure_edge_gateway_services_spec.rb
+++ spec/integration/edge_gateway/configure_edge_gateway_services_spec.rb
@@ -50,6 +50,159 @@ module Vcloud
         actual_config = edge_gateway.vcloud_attributes[:Configuration][:EdgeGatewayServiceConfiguration]
         actual_config[:FirewallService].should == configuration[:FirewallService]
       end
+
+      it "configures a load balancer service" do
+        network_1        = ENV['VCLOUD_NETWORK1_NAME']
+        network_1_id     = ENV['VCLOUD_NETWORK1_ID']
+        load_balancer_ip = ENV['VCLOUD_LOAD_BALANCER_IP']
+
+        configuration = {
+            :LoadBalancerService =>
+                {
+                    :IsEnabled => "true",
+                    :Pool =>
+                      [
+                        {
+                          :Name           =>   "Test pool",
+                          :Description    =>   "Generated from edge_gateway integration tests",
+                          :ServicePort    =>
+                            [
+                              {
+                                :IsEnabled        => "true",
+                                :Protocol         => "HTTP",
+                                :Algorithm        => "ROUND_ROBIN",
+                                :Port             => "80",
+                                :HealthCheckPort  => "",
+                                :HealthCheck      =>
+                                  {
+                                    :Mode               => "HTTP",
+                                    :Uri                => "/",
+                                    :HealthThreshold    => "2",
+                                    :UnhealthThreshold  => "3",
+                                    :Interval           => "5",
+                                    :Timeout            => "15",
+                                  }
+                              },
+                              {
+                                :IsEnabled        => "true",
+                                :Protocol         => "HTTPS",
+                                :Algorithm        => "ROUND_ROBIN",
+                                :Port             => "443",
+                                :HealthCheckPort  => "",
+                                :HealthCheck      =>
+                                  {
+                                    :Mode               => "SSL",
+                                    :Uri                => "",
+                                    :HealthThreshold    => "2",
+                                    :UnhealthThreshold  => "3",
+                                    :Interval           => "5",
+                                    :Timeout            => "15",
+                                  }
+                              },
+                              {
+                                :IsEnabled        => "true",
+                                :Protocol         => "TCP",
+                                :Algorithm        => "ROUND_ROBIN",
+                                :Port             => "999",
+                                :HealthCheckPort  => "",
+                                :HealthCheck      =>
+                                  {
+                                    :Mode               => "TCP",
+                                    :Uri                => "",
+                                    :HealthThreshold    => "2",
+                                    :UnhealthThreshold  => "3",
+                                    :Interval           => "5",
+                                    :Timeout            => "15",
+                                  }
+                              }
+                            ],
+                          :Member         =>
+                            [
+                              {
+                                :IpAddress   => "10.10.10.10",
+                                :Weight      => "1",
+                                :ServicePort =>
+                                  [
+                                    {
+                                      :Protocol => "HTTP",
+                                      :Port     => "",
+                                      :HealthCheckPort => "",
+                                    },
+                                    {
+                                      :Protocol => "HTTPS",
+                                      :Port     => "",
+                                      :HealthCheckPort => "",
+                                    },
+                                    {
+                                      :Protocol => "TCP",
+                                      :Port     => "",
+                                      :HealthCheckPort => "",
+                                    }
+                                  ]
+                              }
+                            ],
+                            :Operational  => "false",
+                          }
+                        ],
+                    :VirtualServer  =>
+                      [
+                        {
+                          :IsEnabled      => "true",
+                          :Name           => "Test virtual server",
+                          :Description    => "Test virtual server",
+                          :Interface      =>
+                              {
+                                :type => "application/vnd.vmware.vcloud.orgVdcNetwork+xml",
+                                :name => network_1,
+                                :href => "https://api.vcd.portal.skyscapecloud.com/api/admin/network/" + network_1_id,
+                              },
+                          :IpAddress      => load_balancer_ip,
+                          :ServiceProfile =>
+                            [
+                              {
+                                :IsEnabled    => "true",
+                                :Protocol     => "HTTP",
+                                :Port         => "80",
+                                :Persistence  =>
+                                  {
+                                      :Method => "",
+                                  }
+                              },
+                              {
+                                :IsEnabled    => "false",
+                                :Protocol     => "HTTPS",
+                                :Port         => "443",
+                                :Persistence  =>
+                                  {
+                                      :Method => "",
+                                  }
+                              },
+                              {
+                                :IsEnabled    => "false",
+                                :Protocol     => "TCP",
+                                :Port         => "999",
+                                :Persistence  =>
+                                  {
+                                      :Method => "",
+                                  }
+                              }
+                            ],
+                          :Logging => "false",
+                          :Pool => "Test pool",
+                        }
+                      ]
+                }
+        }
+        edge_gateway = EdgeGateway.get_by_name(ENV['VCLOUD_EDGE_GATEWAY'])
+        edge_gateway.update_configuration(configuration)
+
+        # Modify our input configuration slightly to match the configuration we're expecting
+        expected_config = configuration
+        expected_config[:LoadBalancerService][:Pool][0][:Operational] = "false"
+
+        actual_config = edge_gateway.vcloud_attributes[:Configuration][:EdgeGatewayServiceConfiguration]
+        actual_config[:LoadBalancerService].should == configuration[:LoadBalancerService]
+      end
     end
   end
 end
